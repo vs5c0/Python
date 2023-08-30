5776f8e29bccdc015b1d6879266afd85b93b491f
sai <127922969+vs5c0@users.noreply.github.com>
2023-08-30 20:17:12 +0530

    Update MatLib Game.py

--- MatLib Game.py
+++ MatLib Game.py
@@ -10,3 +10,15 @@ print("My Fav Place is" + Country)
 print("I like Food Such as  " + Food)
 print("He is My Close Friend in My life " + Friend)
 print("This Book Would change my life Totally " + Book)
+
+#OutPut
+Enter Your Fav Color:Blue
+Enter Your Fav Country:India
+Enter Your Fav Food:Pulihora
+Enter Your Fav Close Friend:Ram
+Enter Your Fav Book:Power of Mind
+I like Blue
+My Fav Place isIndia
+I like Food Such as  Pulihora
+He is My Close Friend in My life Ram
+This Book Would change my life Totally Power of Mind
