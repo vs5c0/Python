d609e2ffc23b42e620768febdd480df1aee77d8c
sai <127922969+vs5c0@users.noreply.github.com>
2023-08-30 18:24:34 +0530

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,5 @@
+# Python is a widely-used, high-level programming language known for its simplicity and readability.
+# It's often used for various purposes including web development, scientific computing, data analysis, artificial intelligence.
+
+
+print("Welcome to The Andhra Pradesh")
