899384b0df995323a2a0de18a5890894c23f8f49
sai <127922969+vs5c0@users.noreply.github.com>
2023-08-30 20:19:14 +0530

    Update Userinput.py

--- Userinput.py
+++ Userinput.py
@@ -4,3 +4,10 @@ age = input("Enter Your age:")
 JobRole = input("Enter Your Job Role:")
 
 print(" Hello " + name + " Your age is " + age + " Your JobRole is  " + JobRole)
+
+
+#OutPut
+Enter Your Name:Kiran
+Enter Your age:21
+Enter Your Job Role:WEd Devloper
+ Hello Kiran Your age is 21 Your JobRole is  WEd Devloper
