85dbcabf34ef46e821889c7ee15dd982c6ac3186
sai <127922969+vs5c0@users.noreply.github.com>
2023-08-30 20:14:28 +0530

    Add files via upload

---
+++ BasicCalculator.py
@@ -0,0 +1,16 @@
+num = input("Enter a Number:")
+num1 = input("Enter another  Number:")
+result = int(num)+int(num1)
+print(result)
+
+result = float(num)+float(num1)
+print(result)
+
+result = int(num)-int(num1)
+print(result)
+
+result = int(num)*int(num1)
+print(result)
+
+result = int(num)%int(num1)
+print(result)
