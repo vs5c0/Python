43f40eb05faff65916d4100545a48516affd7231
sai <127922969+vs5c0@users.noreply.github.com>
2023-08-31 22:28:35 +0530

    Add files via upload

---
+++ Lists/List.py
@@ -0,0 +1,14 @@
+
+# List in Python
+
+Friends = ["Kiran", "Mani", "Ravi"]
+print(Friends)
+# Update the List
+Friends[2] = "Vikram"
+print(Friends)
+
+# Get Value by Using Index
+print(Friends[1])
+
+print(Friends[1:])
+
---
+++ Lists/ListFunctions.py
@@ -0,0 +1,45 @@
+
+Lucky_numbers = [1, 45, 6, 7, 3, 3, 634, 2]
+friends = ["Kiran", "Ram", "Vamsi", "Pavan", "Hyder", "Ram"]
+print(friends)
+
+# Extend Method in List
+friends.extend(Lucky_numbers)
+print(friends)
+
+# Append Method in List
+friends.append("Oscar")
+print(friends)
+
+# Insert Method in List
+friends.insert(3, "Vikram")
+print(friends)
+
+# Count Method in List
+friends.count("Lucky_numbers")
+print(friends)
+
+# Pop Method in List
+friends.pop(2)
+print(friends)
+
+
+# reverse Method in List
+friends.reverse()
+print(friends)
+
+# Copy Method in List
+friends2 = friends.copy()
+print(friends2)
+
+
+
+# Remove Method in List
+friends.remove("Pavan")
+print(friends)
+
+# Clear Method in List
+friends.clear()
+print(friends)
+
+
