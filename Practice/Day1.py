a3affb87ac8340c320687dbc80a4881f47c53d84
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-07 14:10:45 +0530

    Add files via upload

---
+++ Practice/Day1.py
@@ -0,0 +1,23 @@
+# 1 Print Our Name
+
+print("Venkat Sai")
+
+# 2 Print the Add of Two Numbers
+
+print(1+2)
+
+
+# 3 Print the Sub of Two Numbers
+
+print(12-10)
+
+# 4 Print the Mul of Two Numbers
+
+print(12*13)
+
+# 5 Print the Div of Two Numbers
+
+print(12/2)
+
+
+
---
+++ Practice/Day2.py
@@ -0,0 +1,53 @@
+# 1 Declare a & b print sum of a & b
+import math
+
+a = 10
+b = 13
+print(a+b)
+# 2 Print greetings to our name
+name = "Venkat Sai"
+greeting = "Welcome ! "
+print(name+" "+greeting)
+
+# 3 print pi value
+pi = 3.14159
+print(pi)
+
+# print boolean
+is_raining = True
+is_rain = False
+print(is_raining)
+
+# 4 print sentence 3 times
+Sentence = "Welcome to the Bharat     "
+print(Sentence*3)
+
+#  5 add,sub,div,mul
+x=10
+y=9
+print(x+y)
+
+x=10
+y=9
+print(x-y)
+
+x=10
+y=9
+print(x*y)
+
+x=10
+y=9
+print(x/y)
+
+# 6 power of this number
+x = pow(12,12,12)
+
+# 7 floor function
+print(math.floor(2.9))
+
+# 8  remainder function
+print(math.remainder(9,2))
+
+# 9 update the User input
+value = input("Enter the Number : ")
+print("your number is : " +value)
