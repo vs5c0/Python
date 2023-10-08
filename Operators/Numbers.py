baced97cad357a5838e01e34b89d5ee677380f54
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-08 14:22:36 +0530

    Add files via upload

---
+++ Operators/Numbers.py
@@ -0,0 +1,47 @@
+import math
+
+print(2+3)
+
+print(3%1)
+
+my_num = 123
+print(my_num)
+
+# Convert Number into String
+
+my_num1 = 7
+print(str(my_num1)+ " is My Fav Number")
+
+
+print(3*(45+1))
+
+print((3*4)+5)
+
+# abs Function Display Absolute value
+my_num2 = -234
+print(abs(my_num2))
+
+# pow() function return the power of 2 numbers
+print(pow(3,2))
+
+# print max value
+print(max(12, 12, 121))
+
+# print min value
+print(min(12, 23))
+
+#  round math function print nearest  value
+print(round(1.9))
+
+#  floor math function print largest  value
+
+print(math.floor(1.9))
+
+#  ceil math function print  smallest   value
+print(math.ceil(1.9))
+
+#  sqrt math function print square value
+print(math.sqrt(11))
+
+# remainder math function return remainder
+print(math.remainder(12,3))
