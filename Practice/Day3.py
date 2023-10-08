8fc2c5b5147946ae669d292cf45c2d2edd682801
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-08 14:21:50 +0530

    Add files via upload

---
+++ Practice/Day3.py
@@ -0,0 +1,22 @@
+result = 10 * 4 + 12 +(134*1)
+print(result)
+
+# 1 Example
+res = 10+3*2-8/4
+print(res)
+
+# 2 Example
+
+res1 = 4**2+5/2*3
+print(res1)
+
+# 3 Example
+print((8+4)*3/2)
+
+# 4 Example
+res3 = 16/4+2**6-3
+print(res3)
+
+# 5 Example
+res4 = 10-3*(4+2)/5
+print(res4)
