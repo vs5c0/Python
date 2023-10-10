dc118b64a99ae6baeb6be52d0e58358280ee2ebc
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-10 15:06:20 +0530

    Add files via upload

---
+++ Practice/Day4.py
@@ -0,0 +1,78 @@
+
+name = " Nihar" + " " + " is an Amazing Coder"
+print(name)
+
+Name = "Hi Vikram \n" * 5
+print(Name)
+
+s = "Rama Vidya"
+s = ("*  "*3) + s +(" * " * 3)
+print(s)
+
+name = "Vikram Adithya"
+print(len(name))
+
+# 1 Concate 2 Strings
+
+str1 = "Hello"
+str2 = "World"
+print(str1 + str2)
+
+# 2 length of a string
+String = "Bharat is a Great Sprit Land"
+print(len(String))
+
+# 3 Print String  5 times
+
+string = "python " * 5
+print(string)
+
+# 4 length of both strings
+str1 = "Hello"
+print(len(str1))
+str2 = "World"
+print(len(str2))
+s = (str1 + str2)
+print(len(s))
+
+
+# 5 Create a Pattern
+print("*")
+print("**")
+print("***")
+print("*****")
+print("******")
+print("*******")
+
+# 6 print sentences 3 times
+
+Word = input("Enter a Word : ")
+number = Word*3
+print(Word + " Your enter : "+number)
+
+# 7 User inputs and Concate
+
+Word1 = input("Enter the Word : ")
+Word2 = input("Enter the 2nd Word : ")
+print(Word1+" "+Word2)
+
+# 8 print last character of a Sentence
+
+Sent = input("Enter a Sentence : ")
+print(len(Sent))
+print(Sent.index("a"))
+
+
+# 9 Take User input a say greetings
+
+name = input("Enter Your Name: ")
+greetings = input("I says to Your :")
+print(name + " Your are so " +greetings)
+
+
+
+
+
+
+
+
