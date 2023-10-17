5cb908c92784babd410ba24e426d47764e4bc9b4
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-17 20:20:16 +0530

    Add files via upload

---
+++ Practice/Day5.py
@@ -0,0 +1,68 @@
+# TypeConversion -- Convert one datatype into another datatype
+
+
+#  Convert String to int
+num = "123"
+print(type(num))
+num = int(num)
+print(type(num))
+
+# Convert Float to int
+um = 2.34
+print(type(um))
+um = int(num)
+print(type(um))
+
+
+
+# Convert float to String
+num2 = 23.3
+print(type(num2))
+num2 = str(num2)
+print(type(num2))
+
+# Convert  int to string
+um1 = 2
+print(type(um1))
+um1 = str(num)
+print(type(um1))
+
+# 1 Convert integer to string
+result6 = 42
+results = str(result6)
+print(results)
+
+
+# 2 Convert string to integer
+result5 = "42"
+resu = int(result5)
+print(resu)
+
+
+# 3 Convert float into integer
+result4 = 3.14
+result4 = int(result4)
+print(result4)
+
+
+# 4 Convert string to floating point
+result3 = "42.5"
+result3 = float(result3)
+print(result3)
+
+# 5 Convert  integer into boolean
+result2 = 100
+result2 = bool(result2)
+print(result2)
+
+#  6 Convert bool into  integer
+result1 = True
+result1 = int(result1)
+print(result1)
+
+
+# 7 Convert string to boolean
+result = "False"
+result = bool(result)
+print(result)
+
