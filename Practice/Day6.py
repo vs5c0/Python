c54e56516491841d88d090a6f48d090e77db1e94
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-18 20:18:05 +0530

    Add files via upload

---
+++ Practice/Day6.py
@@ -0,0 +1,95 @@
+# String Indexing ---> used to extract the particular character in a String
+
+# Positive indexing
+string = "Vikram Aditya"
+string1 = string[2]
+print(string1)
+
+# Negative indexing
+stri = "Vikram Aditya"
+string2 = stri[-2]
+print(string2)
+
+# Example Program
+Name = "Vikram"
+five = Name[-1]
+four = Name[-2]
+Three = Name[-3]
+Two = Name[-4]
+one = Name[-5]
+zero = Name[-6]
+print(five)
+print(four)
+print(Three)
+print(Two)
+print(one)
+print(zero)
+
+# Slicing in Python
+# Slicling --> is a Process of extract the particular portion of a String
+
+# Example Program
+Names = "Sai Ram"
+sub = Names[0:4]
+print(sub)
+
+# Example program 2
+Navy = "Indian"
+subs = Navy[0:5]
+print(subs)
+
+
+# 1 Extract  First character of a String
+suns = "The sun is shining"
+moon = suns[0]
+print(moon)
+
+# 2 Extract  last character of a String
+sunss = "She sells seashells by the seashore."
+moons = sunss[-1]
+print(moons)
+
+# 3 Extract the  character at the index 3
+
+uns = "I love Python!"
+oons = uns[3]
+print(oons)
+
+# 4 Extract the last character at the sentence
+
+unss = "Life is beautiful."
+oonss = unss[-2]
+print(oonss)
+
+# 5 Extract the substring form index 7 to 14
+
+input = "Welcome to Python Programming"
+s = input[7:14]
+print(s)
+
+# 6 Extract the substring form index -9 to -3
+
+inputs = "The future is bright."
+s1 = inputs[-9:-3]
+print(s1)
+
+# 7 Extract the first six characters of a Sentence
+
+Inp = "Good things take time"
+s2 = Inp[0:6]
+print(s2)
+
+
+# 8 Reverse the sentence Using Slicing
+
+Insps = "Python is awesome!"
+Sao = Insps[-1:12]
+print(Sao)
+
+# Length of a String using index
+
+Form = "Coding is fun!"
+print(len(Form))
+
+
+
