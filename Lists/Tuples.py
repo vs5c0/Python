19e793ee34ed0fa9cac1d6da77afe1e855127b23
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-21 21:34:30 +0530

    Add files via upload

---
+++ Lists/Tuples.py
@@ -0,0 +1,10 @@
+
+# Tuples ---> tuples are immutable used to store the values and seperated by comma with () Paraenthesis
+
+# 1 Example
+pure = (7,9,10,11)
+print("Pure :", pure)
+
+# 2 Example
+my_tuple = ([1,2,3])
+print(my_tuple)
