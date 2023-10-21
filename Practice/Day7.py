f3d9de9400b7501dfbaf0091ba0226c8aaa8a500
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-21 21:34:59 +0530

    Add files via upload

---
+++ Practice/Day7.py
@@ -0,0 +1,99 @@
+# Relational Operators Example
+
+x = 109
+y = 123
+print("Equal to Operator: ", x==y)
+print("Not Equal to Operator: ", x!=y)
+print("Greater than Operator: ", x>y)
+print("Less than  Operator: ", x<y)
+print("Greater than are Equal to Operator: ", x>=y)
+print("Less than are Equal to Operator: ", x<=y)
+
+# 2 Example
+
+a = 5
+b = 2-7
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a<b)
+print(a>b)
+
+
+# 1 Example Greater than operator
+m = 5
+n = 3
+print("Greater than : ", m>n)
+
+# 2 Example Less than operator
+m1 = 10
+n1 = 20
+print("Less than : ", m1<n1)
+
+# 3 Example Greater than  equal to operator
+m2 = 7
+n2 = 7
+print("Greater than are Equal to : ", m2>=n2)
+
+# 4 Example less than are equal to operator
+m3 = 15
+n3 = 12
+print("Less than are Equal to : ", m3<=n3)
+
+# 5 Equal to Operator
+st = "Hello"
+st1 = "Hello"
+print("Equal to :", st==st1)
+
+# 6  Not Equal to Operator
+x = 10
+st = 20
+print("Not Equal to :", x!=st)
+
+# 7 Comparing int and float values
+h = 5.0
+v = 4
+print("Comparing int and float :", h>v)
+
+# 8 Comparing Strings with different cases
+h1 = "Hello"
+v1 = "hello"
+print("Comparing Strings :", h1==v1)
+
+# 9 Using booleans
+fo = True
+eo = False
+print("Booleans : ",fo==eo)
+
+# 10 Comparing Strings
+
+String = "Apple"
+String2 = "Banana"
+print("Comparing Strings: ",String<String2)
+
+# 11 Comparing with none
+go = "none"
+vo = "Python"
+print("Comparing with None :",go==vo)
+
+# 12 Mixing different types
+ur = 5
+ut = "4"
+print("Comparing different values :", ur==ut)
+
+# 13 Using relational operators with negative numbers
+oy = -10
+us = -5
+print("Comparing the neg values: ", oy<us)
+
+# 14 Comparing a string with a number
+fom = "42"
+foes = 42
+print("Comparing String with numbers :" ,fom==foes)
+
+# 15 floating represention
+
+ghn = 0.1+0.1+0.1
+vom = 0.3
+print("Floating Comparision :",ghn==vom)
