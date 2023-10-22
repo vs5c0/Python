5d556152b9c76a2383d484a927934f390e194514
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-22 14:47:01 +0530

    Add files via upload

---
+++ Functions/Functions.py
@@ -0,0 +1,19 @@
+# Function --> is a block of code which is used to perform a specific task
+# There are two types of Functions are there.They are
+# Built in Functions
+# User Defined Functions
+
+# Creating a Function
+def fun():
+    print("Hello ")
+
+#  calling a Function
+def revm():
+    print("Welcome to Bharat")
+revm()
+
+# Defining Parameters to a Function
+def vivo(name,age):
+    print("Hello " +name+ " You age :" +str(age))
+vivo("vikram",21)
+
---
+++ Functions/Return.py
@@ -0,0 +1,12 @@
+# Return Stmt --> used to return back from the functions
+
+# Example
+def cube(num):
+      return num*num*num
+print(cube(7))
+
+# Example
+def square(nums):
+     return nums*nums
+result = square(4)
+print(result)
