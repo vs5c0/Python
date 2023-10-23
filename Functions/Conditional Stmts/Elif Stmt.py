1fa4fef6d7c1d9f90508a6d85faf9b25829c6b02
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-23 15:19:06 +0530

    Add files via upload

---
+++ Functions/Conditional Stmts/Elif Stmt.py
@@ -0,0 +1,14 @@
+#Elif Stmt ---> used only if multiple stmts having
+
+is_Male = False
+is_tall = False
+is_Worked = False
+
+if is_Male:
+    print("Your are a Male")
+elif is_tall:
+    print("Your are a Tall")
+elif is_Worked:
+    print("Your are Worked Professional")
+else:
+    print("Your are not a Human")
