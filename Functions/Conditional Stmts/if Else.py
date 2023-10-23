34a6d62e16c6bc56a638ec6fe9e52f8c86d5dda8
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-23 15:18:41 +0530

    Add files via upload

---
+++ Functions/Conditional Stmts/if Else.py
@@ -0,0 +1,7 @@
+# if Else Stmt --> Used to Execute a block of code if condition is true true block of code excuted otherwise false block is executed
+
+is_Male = False
+if is_Male:
+    print("Your are a Male")
+else:
+    print("Your are not a Male")
