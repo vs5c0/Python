8854c856226f1bbfc8504fc9804fda7f3c3ba2c9
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-27 14:01:26 +0530

    Add files via upload

---
+++ Loops/2DList.py
@@ -0,0 +1,11 @@
+# 2D List ---> a list having another lists
+# 2D Lists are used in data Structure to store and manpulate the data
+
+# Example program
+nums_grid = [
+    [1,2,3],  # index 0
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+print(nums_grid[0][1])
