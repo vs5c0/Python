e540fb557bca42d0df53348c47a98b360b6a9375
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-28 20:35:40 +0530

    Add files via upload

---
+++ Py Basic Game & Calcu/BuildingTranslator.py
@@ -0,0 +1,17 @@
+def translate(pharse):
+    translation = " "
+    for letter in pharse:
+        if letter.islower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+          translation = translation + letter
+          return translation
+
+        print(translate(input("Enter a Pharse: ")))
+
+
+
+
