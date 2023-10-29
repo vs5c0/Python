1da1df37ae65db0f759a908a00f3d5bc3003a1ce
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-29 12:26:06 +0530

    Add files via upload

---
+++ Advancepy/Comments.py
@@ -0,0 +1,11 @@
+# Comments are used to write the text about the code.
+# Comments are mainly 2 ways
+# 1 # By using hashtag
+# 2 ''' '''
+
+# This is a program about the hlo world
+print("Hi World")
+'''
+This is an program about the print stmt
+'''
+print("This is World of karma Bhumi")
