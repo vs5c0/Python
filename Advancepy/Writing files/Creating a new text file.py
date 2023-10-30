3a3bac66c27fa044db4fe84244d8816c27499752
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-30 15:14:15 +0530

    Add files via upload

---
+++ Advancepy/Writing files/Creating a new text file.py
@@ -0,0 +1,3 @@
+em_files = open("Emps2.text", "w")
+em_files.write("sai --- Villian")
+em_files.close()
---
+++ Advancepy/Writing files/append().py
@@ -0,0 +1,5 @@
+# append() function is used to add the text in the text file
+
+emps1_files = open("Emps.text", "a")
+emps1_files.write("\n Rama ---  king")
+emps1_files.close()
