797131ccb2056e249ead5fec8e12ff31d0f316a2
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-10-30 15:14:54 +0530

    Add files via upload

---
+++ Advancepy/Writing files/write().py
@@ -0,0 +1,6 @@
+# Writing a files in python is used to write or append the text in the original text files
+# In python, we can  use write () for write the text as a single text in the files
+
+emps_files = open("Emps.text", "w")
+emps_files.write("Vikas - Agent")
+emps_files.close()
